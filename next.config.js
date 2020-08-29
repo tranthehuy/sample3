a4144c7782b6858b121e84a213a6b3f539c05448
huytran <tranthehuy.2nd@gmail.com>
2020-08-30 00:40:59 +0700

    update host client nextjs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   env: {
-    StripePublishableKey: process.env.StripePublishableKey,
+    HOST: process.env.HOST,
   },
 };
